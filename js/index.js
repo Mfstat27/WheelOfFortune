7ae1bbe6d3ddd1c80589c39e2daf77fdbde71db9
Mfstat27 <tracey_thomas@live.com>
2021-02-27 17:22:52 -0500

    fixing my code

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const generateRandomWord = document.createElement("button")
 generateRandomWord.setAttribute("id", "generateRandomWord")
 generateRandomWord.innerText = "Generate A Word"
 const body = document.getElementById("body")
-body.append(generateRandomWord)
+body.appendChild(generateRandomWord)
 generateRandomWord.addEventListener("click", function(e){
     e.preventDefault()
 })
