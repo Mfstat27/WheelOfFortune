e83063589efa94bdaca25d2708b1f24513dbe3c6
Mfstat27 <tracey_thomas@live.com>
2021-02-27 16:54:50 -0500

    added generate button

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const generateRandomWord = document.createElement("button")
 generateRandomWord.setAttribute("id", "generateRandomWord")
 generateRandomWord.innerText("Generate A Word")
-const body = document.getElementsByTagName("body")
+const body = document.querySelector("body")
 body.append(generateRandomWord)
 generateRandomWord.addEventListener("click", function(e){
     e.preventDefault()
